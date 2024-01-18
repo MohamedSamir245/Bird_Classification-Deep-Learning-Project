f36bfcf078a51db095bf04bbc3f704cd2894b933
Mohamed Samir <mohamedsamir2452002@gmail.com>
2024-01-18 16:39:17 +0200

    Initalized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Unleash your inner ornithologist with this deep learning model! Trained on 90,000+ images, it accurately identifies 525+ bird species with over 94% accuracy! Data augmentation and advanced training techniques ensure top-notch performance.',
+    author='Mohamed Samir',
+    license='',
+)
